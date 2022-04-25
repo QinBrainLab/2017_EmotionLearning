4f7b3ac12102bb1e7490d2fb9813d7c7bb3fbac2
YannanZhu25 <55708068+YannanZhu25@users.noreply.github.com>
2022-04-25 13:51:47 +0200

    Delete Img_QualiCheck_Organ directory

--- Img_QualiCheck_Organ/MGZ_1dcm2nii.m
+++
@@ -1,8 +0,0 @@
-clear;clc;
-rawpath='/lustre/iCAN/backup/MGZ/raw_data';
-cd(rawpath);
-files=dir('MGZ*');
-filenames={files.name};
- for i=filenames(1,2:11)
-       unix(sprintf('dcm2nii -n -g %s',i{:}));
- end
